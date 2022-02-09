3bccc9207059edad148107be492fa07aacddb5dc
kimminkyu <kmk50009@naver.com>
2022-02-09 14:06:37 +0900

    Add UseRouter

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,9 +1,16 @@
 //our-domain/news/something-important
-
+import { useRouter } from "next/router";
 import React from "react";
 
-const DetailPage  = () => {
-    return <h1>The Detail Page</h1>
-}
+const DetailPage = () => {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  //send a request to the backend API
+  //the fetch the news item with newsId
+
+  return <h1>The Detail Page</h1>;
+};
 
 export default DetailPage;
