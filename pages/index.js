34d5d078db047206185d445729015fa8b74dc887
kimminkyu <kmk50009@naver.com>
2022-02-09 14:16:26 +0900

    Add Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 const HomePage  = () => {
     return <h1>The Home Page</h1>
+    
 }
 
 export default HomePage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,24 @@
 //our-domain/news
 
-import React from "react";
+import React, { Fragment } from "react";
+import Link from "next/link";
 
-const NewsPage  = () => {
-    return <h1>The News Page</h1>
-}
+const NewsPage = () => {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>
+            Something Else
+        </li>
+      </ul>
+    </Fragment>
+  );
+};
 
 export default NewsPage;
